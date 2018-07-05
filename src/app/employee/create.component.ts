d20e2464fcb0f36d7e6d5ced3791f47c2084a99c
Bilal <bilal492k7@gmail.com>
2018-07-05 10:41:20 +0500

    departmentmodel

--- src/app/employee/create.component.ts
+++ src/app/employee/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {Department} from '../models/department.model';
 
 @Component({
   selector: 'app-create',
@@ -12,6 +13,13 @@ export class CreateComponent implements OnInit {
   isActive:boolean = true;
   department:number=2;
   
+  departments : Department[] = [
+     
+    { id : 1, name : "IT" },
+    { id : 2, name : "Payroll" },
+    { id : 3, name : "HR" }
+  ];
+
   constructor() { }
 
   ngOnInit() {
